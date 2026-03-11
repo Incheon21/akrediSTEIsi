c40a7cddbf59b62d1fff477f02f44ba63f38f63b
agunglucker <iniabi838@gmail.com>
2026-03-11 20:03:35 +0700

    init dashboard prodi

---
+++ frontend/app/page/prodi/dashboard-prodi/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+
+
+export default function DashboardProdi() {
+  return (
+    <div className="min-h-screen bg-gray-100 dark:bg-black p-8">
+        {/* Konten Dashbard tiap prodi: rekap akreditasi, 
+        status pengisian LED/LKPS, rekomendasi tindakan*/}
+    </div>
+  );
+}
